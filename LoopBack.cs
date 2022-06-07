670a3bf2a88fbcd462ea2894a90b74f3ee7865bf
Pitso Khoboko <88023611+PKhoboko@users.noreply.github.com>
2022-06-07 14:58:15 +0200

    Add files via upload

---
+++ LoopBack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using System.ComponentModel;
+using System.Data;
+
+
+namespace WindowsFormsApplication1
+{
+    class LoopBack
+    {
+
+         
+
+        public static List<WaveInCapabilities> Loop()
+        {
+            List<WaveInCapabilities> sources = new List<WaveInCapabilities>();
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                sources.Add(WaveIn.GetCapabilities(i));
+            }
+
+            return sources;
+            
+        }
+       
+    }
+}
