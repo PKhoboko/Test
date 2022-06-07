bc54c3d621d3389ea1b876448b139bc08ed3218c
Pitso Khoboko <88023611+PKhoboko@users.noreply.github.com>
2022-06-07 14:06:26 +0200

    Add files via upload

---
+++ Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Song
+    {
+         public string SongName { get; set; }
+         public string SongOwner { get; set;  }
+         public string DateProduced { get; set; }
+         public bool Edited { get; set; }
+    }
+}
